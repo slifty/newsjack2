54060cb6075cdb9bf0fc8782e44452bfb0c1be16
slifty <slifty@gmail.com>
2013-03-15 16:03:24 -0500

    thumbnails are getting closer to being real

--- pages/remix/create.php
+++ pages/remix/create.php
@@ -19,12 +19,13 @@
 	$html = $remix->getOriginalDOM();
 	
 	// Add in the NewsJack code
-	$injection  = '<script type="text/javascript" src="'.$BASE_DIRECTORY.'hackasaurus/webxray.js" class="webxray"></script>';
+	$injection = '<script type="text/javascript" src="'.$BASE_DIRECTORY.'js/jquery.min.js" charset="utf-8"></script>';
+	$injection .= '<script type="text/javascript" src="'.$BASE_DIRECTORY.'hackasaurus/webxray.js" class="webxray"></script>';
 	$injection .= '<script type="text/javascript">var remix_id = '.$remix->getItemID().';var remix_url = "'.$remix->getOriginalURL().'";var campaignId="'.(isset($_GET['c'])?$_GET['c']:"").'";</script>';
 	$injection .= '<script type="text/javascript" src="'.$BASE_DIRECTORY.'js/html2canvas.js" charset="utf-8"></script>';
 
-	$html = str_replace("<body>","<body>", $html);
-	$html = str_replace("</body>",$injection."</body>", $html);
+	$html = preg_replace("<.*body.*>","<body><div id='newsjack_content'>", $html, 1);
+	$html = str_replace("</body>","</div>".$injection."</body>", $html);
 	
 	echo $html;
 ?>
